884bf1f5a32db03d207e820c00dab3c0da146454
Eric-Zhang190 <109322363+Eric-Zhang190@users.noreply.github.com>
2022-07-30 15:39:55 -0400

    updates
    updates

---
+++ miniprogram/data/list-data.js
@@ -0,0 +1,54 @@
+let list_data = [
+
+  {
+    title: 'Westchester Park 6034',
+    detail_img1: '/images/IMG_5750.jpg',
+    detail_img2: '/images/IMG_5751.jpg',
+    detail_img3: '/images/IMG_5757.jpg',
+    detail_img4: '/images/IMG_5763.jpg',
+    detail_content: '此处有房源介绍-房源1；此处有房源介绍-房源1；此处有房源介绍-房源1；此处有房源介绍-房源1；此处有房源介绍-房源1',
+
+    postId: 0
+  },
+  
+  {
+    title: 'Westchester Park 6002',
+    detail_img1: '/images/IMG_9453.jpg',
+    detail_img2: '/images/IMG_9460.jpg',
+    detail_img3: '/images/IMG_9469.jpg',
+    detail_img4: '/images/IMG_9479.jpg',
+    detail_content: '此处有房源介绍-房源2；此处有房源介绍-房源2；此处有房源介绍-房源2；此处有房源介绍-房源2；此处有房源介绍-房源2；此处有房源介绍-房源2；此处有房源介绍-房源2；',
+
+    postId: 1
+  
+  },
+  
+  // {
+  //   title: 'Westchester Park',
+  //   detail_img1: '/images/1.jpg',
+  //   detail_img2: '/images/2.jpg',
+  //   detail_img3: '/images/3.jpg',
+  //   detail_img4: '/images/5.jpg',
+  //   detail_content: '此处有房源介绍-房源3；此处有房源介绍-房源3；此处有房源介绍-房源3；此处有房源介绍-房源3；此处有房源介绍-房源3；此处有房源介绍-房源3；此处有房源介绍-房源3；此处有房源介绍-房源3；',
+
+  //   postId: 2
+  
+  // },
+  
+  // {
+  //   title: '房源5',
+  //   detail_img1: '/images/1.jpg',
+  //   detail_img2: '/images/2.jpg',
+  //   detail_img3: '/images/3.jpg',
+  //   detail_img4: '/images/5.jpg',
+  //   detail_content: '此处有房源介绍-房源4；此处有房源介绍-房源4；此处有房源介绍-房源4；此处有房源介绍-房源4；此处有房源介绍-房源4；此处有房源介绍-房源4；此处有房源介绍-房源4；此处有房源介绍-房源4；',
+
+  //   postId: 3
+  
+  // }
+  
+  ];
+  
+  module.exports = {list_data};
+  
+  
---
+++ miniprogram/pages/list/list.js
@@ -0,0 +1,96 @@
+// pages/list/list.js
+let datas = require('../../data/list-data.js');
+console.log(datas);
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      listArr: [],
+      items: [
+        {value: 'All', name: 'All', checked: 'true'},
+        {value: 'UMD', name: 'UMD'},
+        {value: 'GBS', name: 'GBS'},
+      ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      listArr : datas.list_data
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  radioChange(e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value)
+
+    const items = this.data.items
+    for (let i = 0, len = items.length; i < len; ++i) {
+      items[i].checked = items[i].value === e.detail.value
+    }
+
+    this.setData({
+      items
+    })
+  },
+
+  toDetails(){
+    wx.navigateTo({
+      url: 'pages/d',
+    })
+  }
+
+
+})
